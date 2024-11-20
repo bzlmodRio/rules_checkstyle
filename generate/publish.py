1931b1054cb89773801597ca1aa7308381e3f9b4
PJ Reiniger <pj.reiniger@gmail.com>
2024-11-20 00:07:05 -0500

    Lint

--- generate/publish.py
+++ generate/publish.py
@@ -1,7 +1,8 @@
 import os
+
 from bazelrio_gentool.publish_module import publish_module
-from get_checkstyle_group import get_checkstyle_group
 from bazelrio_gentool.utils import TEMPLATE_BASE_DIR
+from get_checkstyle_group import get_checkstyle_group
 
 
 def main():
